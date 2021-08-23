71fa3d45b9817654a3b13602976b74186416d6f3
vanderson nogueira <vanderson.01@hotmail.com>
2021-08-23 15:17:53 -0300

    find by email

--- src/main/java/com/wallet/repositoy/UserRepository.java
+++ src/main/java/com/wallet/repositoy/UserRepository.java
@@ -1,9 +1,14 @@
 package com.wallet.repositoy;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wallet.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	/*Find By ? Email By cpf By ... */
+	Optional<User> findByEmail(String email);
 
 }
--- src/test/java/com/wallet/repositoy/UserRepositoryTest.java
+++ src/test/java/com/wallet/repositoy/UserRepositoryTest.java
@@ -1,7 +1,12 @@
 package com.wallet.repositoy;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Optional;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,20 +21,48 @@ import com.wallet.models.User;
 @ActiveProfiles("test")
 public class UserRepositoryTest {
 
+	private static final String EMAIL = "setup@teste.com";
 	@Autowired
 	UserRepository repository;
 
+	@Before
+	public void setUp() {
+		User u = new User();
+		u.setName("Set up user");
+		u.setPassword("123456");
+		u.setEmail(EMAIL);
+		u.setCpf("123123123-12");
+
+		repository.save(u);
+
+	}
+
+	/***
+	 * Deleta tudo que o repositório gerencia após a execução do test.
+	 */
+	@After
+	public void tearDown() {
+		repository.deleteAll();
+	}
+
 	@Test
 	public void testSave() {
-		 User u = new User();
-		 u.setName("Teste");
-		 u.setPassword("123456");
-		 u.setEmail("teste@teste.com");
-		 u.setCpf("123123123");
-	
-		 User response =  repository.save(u);
-		
+		User u = new User();
+		u.setName("Teste");
+		u.setPassword("123456");
+		u.setEmail(EMAIL);
+		u.setCpf("123123123");
+
+		User response = repository.save(u);
+
 		assertNotNull(response);
 	}
-	
+
+	@Test
+	public void testFindByEmail() {
+		Optional<User> response = repository.findByEmail(EMAIL);
+
+		assertTrue(response.isPresent());
+		assertTrue(response.get().getEmail().equals(EMAIL));
+	}
 }
